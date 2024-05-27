3324d318f0de831e1e300ae373675c7c0c3baeab
Nady1303 <ohlebushkina@yandex.ru>
2024-05-27 22:12:02 +0400

    voices pyttsx3

--- all/voice.py
+++ all/voice.py
@@ -1,33 +1,9 @@
-# import speakerpy
+import pyttsx3
 
-VOICE_ID = 'rxEz5E7hIAPk7D3bXwf6'
-CHUNK_SIZE = 1024
-API_KEY_VOICE = '3f2fd6f1b6f41e9bd13d3aa26ca34f7b'
-url = f"https://api.elevenlabs.io/v1/text-to-speech/{VOICE_ID}"
-
-headers = {
-    "Accept": "audio/mpeg",
-    "Content-Type": "application/json",
-    "xi-api-key": API_KEY_VOICE
-}
-
-
-def speach(text: str, filename='output1', speed=1.5) -> str:
-    data = {
-        "text": text,
-        'language': 'ru',
-        "model_id": "eleven_multilingual_v2",
-        "voice_settings": {
-            "stability": 0.5,
-            "similarity_boost": 0.5,
-            'speed': speed
-
-        }
-    }
-
-    response = requests.post(url, json=data, headers=headers)
-    with open(f'{filename}.mp3', 'wb') as f:
-        for chunk in response.iter_content(chunk_size=CHUNK_SIZE):
-            if chunk:
-                f.write(chunk)
-    return f'{filename}.mp3'
+def speach(text: str, filename='output1') -> str:
+    engine = pyttsx3.init()
+    text = text
+    engine.setProperty("rate", 150)
+    engine.save_to_file(text, f"{filename}.mp3")
+    engine.runAndWait()
+    # return f"{filename}.mp3"
