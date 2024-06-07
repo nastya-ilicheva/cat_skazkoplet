0e4b75088c59b80cc0d0c3ce8363d5f4ba232e5d
Sofi-s <122690888+Sofi-s@users.noreply.github.com>
2024-06-07 15:18:29 +0300

    промты и дизайн кнопок

--- candinsky_and_gigachat/generate_prompt_for_kandy.py
+++ candinsky_and_gigachat/generate_prompt_for_kandy.py
@@ -4,7 +4,7 @@ from candinsky_and_gigachat.giga import *
 def create_prompt(chat, msg):
     # messages = [SystemMessage(content="Ты упрощаешь введенный текст. Убери все ненужное, но оставь описания.")] + msg
     messages = [SystemMessage(content="Ты - редактор. Ты упрощаешь и сокращаешь текст последнего предложения отрывка сказки. страшное содержание делаешь менее страшным. "
-                                      " Никогда не придумывай нового.")]
+                                      " Никогда не придумывай нового. Если ты встречаешь иена исторических персонажей, оставляй описание персонажа и дай ему новое имя ")]
     messages.append(HumanMessage(content=msg))
     print("prompt", messages)
     res = chat(messages)
